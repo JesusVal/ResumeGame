ea9a0204c2ec4393dd0fcdf1b09ade8cedf32583
JesusVal <jesus.valadez.f.97@gmail.com>
2022-07-24 22:04:05 -0500

    Hello world in three

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './pages/Game';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Game/>
   </React.StrictMode>
 );
 
