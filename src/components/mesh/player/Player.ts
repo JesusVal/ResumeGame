43ff8f95406231871a4bc0f1f0bc4cc94dc5804e
JESUS ANTONIO VALADEZ FLORES <jesus.valadez@lubtrac.com.mx>
2022-08-09 14:26:56 -0500

    Player function creation

--- src/components/mesh/player/Player.ts
+++ src/components/mesh/player/Player.ts
@@ -1,25 +1,29 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-export function Player() {
-    const chicken = new THREE.Group();
-  
-    // const body = new THREE.Mesh(
-    //   new THREE.BoxBufferGeometry( chickenSize*zoom, chickenSize*zoom, 20*zoom ), 
-    //   new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } )
-    // );
-    // body.position.z = 10*zoom;
-    // body.castShadow = true;
-    // body.receiveShadow = true;
-    // chicken.add(body);
-  
-    // const rowel = new THREE.Mesh(
-    //   new THREE.BoxBufferGeometry( 2*zoom, 4*zoom, 2*zoom ), 
-    //   new THREE.MeshLambertMaterial( { color: 0xF0619A, flatShading: true } )
-    // );
-    // rowel.position.z = 21*zoom;
-    // rowel.castShadow = true;
-    // rowel.receiveShadow = false;
-    // chicken.add(rowel);
-  
-    return chicken;  
-  }
+export const PLAYER_SIZE = 15
+
+export function Player(zoom: number, playerColor = 0xffffff) {
+  const player = new THREE.Group()
+
+  const body = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(PLAYER_SIZE * zoom, PLAYER_SIZE * zoom, 20 * zoom),
+    new THREE.MeshPhongMaterial({ color: playerColor, flatShading: true }),
+  )
+
+  body.position.z = 10 * zoom
+  body.castShadow = true
+  body.receiveShadow = true
+  player.add(body)
+
+  const rowel = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(2 * zoom, 4 * zoom, 2 * zoom),
+    new THREE.MeshPhongMaterial({ color: 0xf0619a, flatShading: true }),
+  )
+  rowel.position.z = 21 * zoom
+  rowel.castShadow = true
+  rowel.receiveShadow = false
+
+  player.add(rowel)
+
+  return player
+}
